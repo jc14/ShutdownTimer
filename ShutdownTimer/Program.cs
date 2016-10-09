37144aa6773556fe827534c3f24c8c147e05fd4b
Jordan Campbell <jcampbell1441@gmail.com>
2016-10-08 22:09:43 -0500

    Complete app need to test

--- ShutdownTimer/Program.cs
+++ ShutdownTimer/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ShutdownTimer
 {
@@ -10,6 +11,51 @@ namespace ShutdownTimer
     {
         static void Main(string[] args)
         {
+            int seconds = Start();
+            while(true)
+            {
+                int minutes = (int) (seconds / 60);
+                Console.Clear();
+                Console.WriteLine("Computer set to shutdown in {0} minutes and {1} seconds", minutes, seconds % 60);
+
+                seconds--;
+                if(seconds < 0)
+                {
+                    Shutdown();
+                    break;
+                }
+
+                Thread.Sleep(1000);
+                
+            }
+
+        }
+
+        static int Start()
+        {
+            Console.WriteLine("Windows shutdown timer program by Jordan Campbell");
+            Console.WriteLine("Please enter the amount of minutes you want until your pc will shut off");
+
+            string input = Console.ReadLine();
+            int x = 0;
+            if (Int32.TryParse(input, out x))
+            {
+                Console.WriteLine(x * 60);
+                Console.ReadKey();
+                return x * 60;
+            } else
+            {
+                Console.Clear();
+                Start();
+            }
+            return -1;
+        }
+
+        static void Shutdown()
+        {
+            Console.Clear();
+            Console.WriteLine("Shutdown!");
+            Process.Start("shutdown", "/s /t 0");
         }
     }
 }
